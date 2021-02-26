47b74b6802c86395f26632b7c605e57d5cac84b6
kocteban <kocteban@yandex.ru>
2021-02-26 14:56:28 +0500

    start editing tests

--- Mp3ha/Mp3ha/helper/make_some_cookies.py
+++ Mp3ha/Mp3ha/helper/make_some_cookies.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from locators.BasePage import SITE_URL
 
 #Запись куки
 chrome_options = Options()
 chrome_options.add_argument("user-data-dir=C:\python\Work\Zvonko-test\Zvuch\Zvuch\helper\selenium")
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get("https://www.zvuch.com/")
+driver.get(SITE_URL)
--- Muzofond/Muzofond/helper/make_some_cookies.py
+++ Muzofond/Muzofond/helper/make_some_cookies.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from locators.BasePage import SITE_URL
 
 #Запись куки
 chrome_options = Options()
 chrome_options.add_argument("user-data-dir=C:\python\Work\Zvonko-test\Muzofond\Muzofond\helper\selenium")
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get("https://muzofond.fm/")
+driver.get(SITE_URL)
